0071ea47884700aa49803401a45f6bc0cfba5c79
hearty_dev <facudnoesclante@gmail.com>
2025-09-19 21:34:39 -0300

    modelos: modelo y esquema de models definidos

---
+++ src/models/comment.model.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+// ● _id (ObjectId automático)
+// ● content (String, 5-500 caracteres)
+// ● author (ObjectId, referencia a User)
+// ● article (ObjectId, referencia a Article - relación 1:N)
+// ● createdAt (Date)
+// ● updatedAt (Date)
+const commentSchema = new Schema({
+    content:{
+        type: String,
+        min:5,
+        max:500,
+        require: true
+    },
+    author:{
+        type: Schema.Types.ObjectId,
+        ref: "users",
+        require: true
+    },
+    article:{
+        type: Schema.Types.ObjectId,
+        ref: "articles"
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    updatedAt:{
+        type: Date
+    }
+});
+export const commentModel = model( "comments", commentSchema);
--- src/models/models.js
+++ src/models/models.js
@@ -1,3 +1,4 @@
 import "./user.model.js";
 import "./article.model.js";
 import "./tag.model.js";
+import "./comment.model.js";
